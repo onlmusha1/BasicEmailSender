3dd61e5dff8164d2ae7859cb4f0cb4d9dc9b4792
Clayton McDowell <johns7283@gmail.com>
2019-02-19 20:58:16 -0500

    Add project files.

---
+++ Email Sender/EmailSender.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Net;
+using System.Net.Mail;
+using System.Security;
+using System.Configuration;
+using System.Configuration.Assemblies;
+using System.Collections.Generic;
+using System.Text;
+using System.Collections;
+
+namespace Email_Sender
+{
+    class EmailSender
+    {
+
+        public SmtpClient emailClient = new SmtpClient("smtp.gmail.com", 587);
+        private MailMessage mailMessage;
+        public EmailSender()
+        {
+            this.emailClient.UseDefaultCredentials = false;
+            this.emailClient.Credentials = new NetworkCredential(
+                ConfigurationManager.ConnectionStrings["gmail"].ProviderName,
+                new NetworkCredential("", ConfigurationManager.ConnectionStrings["gmail"].ConnectionString).SecurePassword
+                );
+            this.emailClient.Timeout = 3000;
+            this.emailClient.EnableSsl = true;
+        }
+
+        public void sendEmail(string body, string subject, string toEmail)
+        {
+            this.mailMessage = new MailMessage("lovetrumpshate6969@gmail.com", toEmail, subject, body);
+            try
+            {
+                this.emailClient.SendMailAsync(this.mailMessage);
+            }
+            catch(Exception ex)
+            {
+                this.emailClient.Dispose();
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+    }
+}
---
+++ Email Sender/Form1.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Mail;
+using System.Windows.Forms;
+
+namespace Email_Sender
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private EmailHandler emailHandler = new EmailHandler();
+
+        private void btnSend_Click(object sender, EventArgs e)
+        {
+            this.emailHandler.addEmailToQueue(new Email(this.txtBody.Text, this.txtSubject.Text, this.txtSendTo.Text));
+            
+        }
+        
+    }
+    internal class EmailHandler
+    {
+        private EmailSender email = new EmailSender();
+        private List<Email> emailQueue = new List<Email>();
+        public EmailHandler()
+        {
+            this.email.emailClient.SendCompleted += MessageSent;
+        }
+
+        public void addEmailToQueue(Email email)
+        {
+            this.emailQueue.Add(email);
+            if (this.emailQueue.Count > 0)
+            {
+                this.trySendEmail();
+            }
+        }
+
+        private void trySendEmail()
+        {
+            if (this.emailQueue.Count > 0)
+            {
+                this.email.sendEmail(this.emailQueue[0].Body, this.emailQueue[0].Subject, this.emailQueue[0].ToAddress);
+            }
+        }
+        private void MessageSent(object sender, EventArgs e)
+        {
+            this.emailQueue.RemoveAt(0);
+            if(this.emailQueue.Count > 0)
+            {
+                this.trySendEmail();
+            }
+        }
+    }
+
+    internal class Email
+    {
+        private string body;
+        private string subject;
+        private string toAddress;
+
+        public string Body { get => body; set => body = value; }
+        public string Subject { get => subject; set => subject = value; }
+        public string ToAddress { get => toAddress; set => toAddress = value; }
+
+        public Email(string body, string subject, string toAddress)
+        {
+            this.Body = body;
+            this.Subject = subject;
+            this.ToAddress = toAddress;
+        }
+    }
+}
